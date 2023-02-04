d5201acc8ca75042ab28a1dba019b72c2e2a13f3
davidlopez <dfl11@alu.ua.es>
2023-02-04 18:27:50 +0100

    Ejercicio de la sección "Sistema de archivos"

--- PruebaArchivos/ViewController.swift
+++ PruebaArchivos/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let file = "prueba.txt"  //nombre del archivo
+        let text = "hola iOS"    //texto que escribiremos en él
+
+        //obtenemos la URL del directorio Documents
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            //le añadimos detrás el "/prueba.txt"
+            let fileURL = dir.appendingPathComponent(file)
+            //guardamos el String en el archivo
+            try! text.write(to: fileURL, atomically: false, encoding: .utf8)
+        }
+
     }
 
 
